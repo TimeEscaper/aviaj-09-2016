f7045f00df80e32fb81ea8e08362100dc56dca94
TimeEscaper <rentgeny05@gmail.com>
2016-12-02 19:43:59 +0300

    Добавлены классы Dot и Vector.

---
+++ src/main/java/ru/aviaj/game/basetype/Dot.java
@@ -0,0 +1,40 @@
+package ru.aviaj.game.basetype;
+
+@SuppressWarnings({"ClassNamingConvention", "unused", "InstanceVariableNamingConvention"})
+public class Dot {
+    private long x;
+    private long y;
+    private long z;
+
+    public Dot() { x = y = z = 0 ; }
+
+    public Dot(long x, long y, long z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+    public void setZ(long z) {
+        this.z = z;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public long getZ() {
+        return z;
+    }
+}
